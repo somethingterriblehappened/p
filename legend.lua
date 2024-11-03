aba1a35ab39537b054a53665610a77f109c5eb07
somethingterriblehappened <backupbackup451@outlook.com>
2024-11-02 20:26:09 -0400

    Update legend.lua

--- legend.lua
+++ legend.lua
@@ -84,7 +84,143 @@ game:GetService("UserInputService").InputBegan:Connect(function(input, gameProce
                 wait(1.4)
                 debounce = false
             elseif game.Players.LocalPlayer.leaderstats.Glove.Value == "Jet" then
-
+                local function InitiateUI_upvr(arg1)
+                    local CursorIMG_upvr = arg1.Frame.CursorIMG
+                    local OutlineIMG_upvr = arg1.Frame.OutlineIMG
+                    game:GetService("TweenService"):Create(OutlineIMG_upvr, TweenInfo.new(1, Enum.EasingStyle.Cubic, Enum.EasingDirection.Out, 0, false, 0), {
+                        Rotation = 90;
+                        ImageTransparency = 0;
+                    }):Play()
+                    game:GetService("TweenService"):Create(CursorIMG_upvr, TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut, 0, false, 0), {
+                        ImageTransparency = 0;
+                    }):Play()
+                    game:GetService("Debris"):AddItem(OutlineIMG_upvr, 5)
+                    game:GetService("Debris"):AddItem(CursorIMG_upvr, 5)
+                    coroutine.resume(coroutine.create(function() -- Line 27
+                        wait(3)
+                        if CursorIMG_upvr and OutlineIMG_upvr then
+                            game:GetService("TweenService"):Create(CursorIMG_upvr, TweenInfo.new(2, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut, 0, false, 0), {
+                                ImageTransparency = 1;
+                            }):Play()
+                            game:GetService("TweenService"):Create(OutlineIMG_upvr, TweenInfo.new(2, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut, 0, false, 0), {
+                                ImageTransparency = 1;
+                            }):Play()
+                        end
+                    end))
+                end
+                local tbl_3 = {}
+                for i, v in pairs(game.Players:GetPlayers()) do
+                    if v:FindFirstChild("leaderstats") ~= nil and v.Character and v.Character.isInArena.Value == true and v.Character.IsInDefaultArena.Value == false then
+                        table.insert(tbl_3, {
+                            Player = v;
+                            Magnitude = (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - v.Character.HumanoidRootPart.Position).Magnitude;
+                        })
+                    end
+                end
+                table.sort(tbl_3, function(arg1, arg2) -- Line 46
+                    local var34
+                    if arg1.Magnitude >= arg2.Magnitude then
+                        var34 = false
+                    else
+                        var34 = true
+                    end
+                    return var34
+                end)
+                if tbl_3[2] ~= nil and tbl_3[2].Player ~= nil then
+                    v = tbl_3[2].Player.Character
+                    i = v.HumanoidRootPart
+                    v = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+                    if (i.Position - v).Magnitude < 100 then
+                        i = game                        
+                        if tbl_3[2].Player ~= i.Players.LocalPlayer then
+                            v = tbl_3[2]
+                            i = v.Player
+                            game:GetService("ReplicatedStorage").AirStrike:FireServer(i.Character)
+                            local clone = game.ReplicatedStorage.JetTarget:Clone()
+                            InitiateUI_upvr(clone)
+                            v = tbl_3[2]
+                            i = v.Player
+                            clone.Parent = i.Character.HumanoidRootPart
+                            game.SoundService.Target:Play()
+                            cooldown(5)
+                        end
+                    end
+                end
+            elseif game.Players.LocalPlayer.leaderstats.Glove.Value == "Hunter" then
+                local function InitiateUI_upvr(arg1)
+                    local CursorIMG_upvr = arg1.Frame.CursorIMG
+                    local OutlineIMG_upvr = arg1.Frame.OutlineIMG
+                    game:GetService("TweenService"):Create(OutlineIMG_upvr, TweenInfo.new(1, Enum.EasingStyle.Cubic, Enum.EasingDirection.Out, 0, false, 0), {
+                        Rotation = 90;
+                        ImageTransparency = 0;
+                    }):Play()
+                    game:GetService("TweenService"):Create(CursorIMG_upvr, TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut, 0, false, 0), {
+                        ImageTransparency = 0;
+                    }):Play()
+                    game:GetService("Debris"):AddItem(OutlineIMG_upvr, 5)
+                    game:GetService("Debris"):AddItem(CursorIMG_upvr, 5)
+                    coroutine.resume(coroutine.create(function() -- Line 27
+                        wait(3)
+                        if CursorIMG_upvr and OutlineIMG_upvr then
+                            game:GetService("TweenService"):Create(CursorIMG_upvr, TweenInfo.new(2, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut, 0, false, 0), {
+                                ImageTransparency = 1;
+                            }):Play()
+                            game:GetService("TweenService"):Create(OutlineIMG_upvr, TweenInfo.new(2, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut, 0, false, 0), {
+                                ImageTransparency = 1;
+                            }):Play()
+                        end
+                    end))
+                end
+                local tbl_3 = {}
+                for i, v in pairs(game.Players:GetPlayers()) do
+                    if v:FindFirstChild("leaderstats") ~= nil and v.Character and v.Character.isInArena.Value == true and v.Character.IsInDefaultArena.Value == false then
+                        table.insert(tbl_3, {
+                            Player = v;
+                            Magnitude = (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - v.Character.HumanoidRootPart.Position).Magnitude;
+                        })
+                    end
+                end
+                table.sort(tbl_3, function(arg1, arg2) -- Line 46
+                    local var34
+                    if arg1.Magnitude >= arg2.Magnitude then
+                        var34 = false
+                    else
+                        var34 = true
+                    end
+                    return var34
+                end)
+                if tbl_3[2] ~= nil and tbl_3[2].Player ~= nil then
+                    i = game                        
+                    if tbl_3[2].Player ~= i.Players.LocalPlayer then
+                        v = tbl_3[2]
+                        i = v.Player
+                        game:GetService("ReplicatedStorage").HunterAbility:FireServer(i.Character)
+                        local clone = game.ReplicatedStorage.JetTarget:Clone()
+                        InitiateUI_upvr(clone)
+                        v = tbl_3[2]
+                        i = v.Player
+                        clone.Parent = i.Character.HumanoidRootPart
+                        local Sound_4 = Instance.new("Sound")
+                        Sound_4.SoundId = "rbxassetid://5701808679"
+                        game:GetService("Debris"):AddItem(Sound_4, 10)
+                        Sound_4.Volume = 2
+                        Sound_4.Parent = game.SoundService
+                        Sound_4:Play()
+                        local Sound_2 = Instance.new("Sound")
+                        Sound_2.SoundId = "rbxassetid://533256819"
+                        game:GetService("Debris"):AddItem(Sound_2, 10)
+                        Sound_2.Volume = 2
+                        Sound_2.Parent = game.SoundService
+                        Sound_2:Play()
+                        local Sound = Instance.new("Sound")
+                        Sound.SoundId = "rbxassetid://1841354127"
+                        game:GetService("Debris"):AddItem(Sound, 10)
+                        Sound.Volume = 2
+                        Sound.Parent = game.SoundService
+                        Sound:Play()
+                        cooldown(5)
+                    end
+                end
             elseif game.Players.LocalPlayer.leaderstats.Glove.Value == "Fort" then
                 game:GetService("ReplicatedStorage").Fortlol:FireServer()
                 cooldown(3)
@@ -125,7 +261,7 @@ game:GetService("UserInputService").InputBegan:Connect(function(input, gameProce
                 if tbl_3[2] ~= nil and tbl_3[2].Player ~= nil then
                     if tbl_3[2].Player ~= LocalPlayer then
                         var5_upvw = true
-                        game.ReplicatedStorage.MagnetS:FireServer()
+                        game:GetService("ReplicatedStorage").MagnetS:FireServer()
                         local Attachment_2 = Instance.new("Attachment")
                         Attachment_2.Parent = LocalPlayer.Character.HumanoidRootPart
                         Attachment_2.Name = "magnetismATTACHMENT"
@@ -150,10 +286,57 @@ game:GetService("UserInputService").InputBegan:Connect(function(input, gameProce
             elseif game.Players.LocalPlayer.leaderstats.Glove.Value == "bus" then
                 game:GetService("ReplicatedStorage").busmoment:FireServer()
                 cooldown(5)
+            elseif game.Players.LocalPlayer.leaderstats.Glove.Value == "Space" then
+                game:GetService("ReplicatedStorage").ZeroGSound:FireServer()
+                game.Workspace.Gravity = 0
+                local ColorCorrectionEffect = Instance.new("ColorCorrectionEffect")
+                ColorCorrectionEffect.Name = "ZeroGColor"
+                ColorCorrectionEffect.Parent = game.Lighting
+                game:GetService("TweenService"):Create(ColorCorrectionEffect, TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut, 0, false, 0), {
+                    TintColor = Color3.new(0.333333, 1, 0);
+                    Brightness = -0.3;
+                }):Play()
+                wait(4)
+                game:GetService("TweenService"):Create(ColorCorrectionEffect, TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut, 0, false, 0), {
+                    TintColor = Color3.new(1, 1, 1);
+                    Brightness = 0;
+                }):Play()
+                game:GetService("TweenService"):Create(game.Workspace, TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut, 0, false, 0), {
+                    Gravity = 196.2;
+                }):Play()
+                wait(1)
+                ColorCorrectionEffect:Destroy()
+            elseif game.Players.LocalPlayer.leaderstats.Glove.Value == "Acrobat" then
+                local Animation_upvr = Instance.new("Animation")
+                Animation_upvr.AnimationId = "rbxassetid://16299510063"
+                game.Players.LocalPlayer.Character.Humanoid.Animator:LoadAnimation(Animation_upvr):Play()
+                game:GetService("ReplicatedStorage").Roll:FireServer()
+                local BodyVelocity = Instance.new("BodyVelocity")
+                BodyVelocity.Velocity = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame.LookVector * 100
+                BodyVelocity.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
+                game:GetService("Debris"):AddItem(BodyVelocity, 0.1)
+                for _, v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+                    if v:IsA("BasePart") then
+                        v.Massless = true
+                    end
+                end
+                wait(0.6)
+                if game.Players.LocalPlayer.Character then
+                    for _, v_2 in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+                        if v_2:IsA("BasePart") then
+                            v_2.Massless = false
+                        end
+                    end
+                end
+            elseif game.Players.LocalPlayer.leaderstats.Glove.Value == "Anchor" then
+                game:GetService("TweenService"):Create(game:GetService("ReplicatedStorage").AnchorActivated:InvokeServer().BillboardGui.ImageLabel, TweenInfo.new(1, Enum.EasingStyle.Cubic, Enum.EasingDirection.Out, 0, false, 0), {
+	                ImageTransparency = 0;
+                }):Play()
+                cooldown(1)
             else
-        --game:GetService("ReplicatedStorage").HunterAbility:FireServer(game.Players[""].Character)
-            game:GetService("ReplicatedStorage").GeneralAbility:FireServer()
-        --game:GetService("ReplicatedStorage").Ghostinvisibilityactivated:FireServer()
+                --game:GetService("ReplicatedStorage").HunterAbility:FireServer(game.Players[""].Character)
+                game:GetService("ReplicatedStorage").GeneralAbility:FireServer()
+                --game:GetService("ReplicatedStorage").Ghostinvisibilityactivated:FireServer()
             end
         end
     end
